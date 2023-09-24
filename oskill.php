9445fd4f4bda1a48023f4e8706b9419d2e6d3ec2
Aurozia <valentineoay@gmail.com>
2023-09-24 23:59:40 +0200

    oSkill.php V1

---
+++ oskill.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Karmine Corp</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-oskill.css">
+    <link rel="shortcut icon" href="images/oskill/skill.png" type="image/x-icon">
+    <?php include 'php/font.php' ?>
+</head>
+
+<body>
+    <main>
+        <?php include 'php/summary.php' ?>
+        <section id="main-section">
+            <section id="main-section-container-page">
+
+                <div id="body-main">
+                    <div id="oskill-container">
+                        <div id="oskill-left">
+                            <header id="oskill-left-header">
+                                <div id="oskill-left-header-help">
+                                    <h1 id="oskill-left-header-title">Mes skills</h1>
+                                </div>
+                            </header>
+                            <main id="oskill-left-main">
+                                <h2 id="oskill-left-main-h2">Valentine YAO <em id="oskill-left-main-subtitle">Futur développeuse</em></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.</p>
+                            </main>
+                            <footer id="oskill-left-footer">
+                                <nav id="oskill-nav">
+                                    <a href="#">À propos de moi</a>
+                                    <a href="#">Mentions légales</a>
+                                    <a href="#">Contact</a>
+                                </nav>
+                            </footer>
+                        </div>
+                        <div id="oskill-right">
+                            <header id="oskill-right-header">
+                                <h1 id="oskill-right-header-h1">Mes compétences</h1>
+                            </header>
+                            <main id="oskill-right-main">
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                                <section class="oskill-right-main-section">
+                                    <article class="oskill-right-main-article">TEXTE</article>
+                                    <article class="oskill-right-main-article">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</article>
+                                </section>
+                            </main>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        </section>
+    </main>
+</body>
+
+</html>
